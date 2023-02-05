70f59ae03fdc6686c5cfcdd6cfb76e99635b7066
Nishizaki <yutaka.w1986@gmail.com>
2023-02-05 14:48:14 +0900

    freertosのテストを追加
    https://qiita.com/azuki_bar/items/d84e504fcd32dddc5c4b
    を参考にfreertosのテストを追加

---
+++ training/1_Task_Static/test/test_main.cpp
@@ -0,0 +1,40 @@
+#include <thread>
+#include "gtest/gtest.h"
+
+extern "C" {
+	#include "TaskCreateStatic.h"
+}
+
+/*-----------------------------------------------------------*/
+
+namespace {
+	
+static void drive_freertos(void);
+
+static void drive_freertos(void){
+	TaskCreateStatic();
+	for (;;);
+}
+
+class FreeRtosUnitTest : public testing::Test {
+protected:
+	void SetUp() override {
+		std::thread th_freertos(drive_freertos);
+		th_freertos.detach();
+	}
+	void TearDown() override {
+	}
+};
+
+TEST_F(FreeRtosUnitTest, Sample1) {
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    int ret =  RUN_ALL_TESTS();
+    return ret;
+}
+
+}  // namespace
