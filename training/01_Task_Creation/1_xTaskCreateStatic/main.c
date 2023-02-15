49559adf653bc47de2ab4c8e74010b73fc46ae8d
Nishizaki <steelpipe75@gmail.com>
2023-02-16 00:04:41 +0900

    タスクスケジューラ起動した後にreturnしてきた場合は異常 終了コードを非0にする

--- training/01_Task_Creation/1_xTaskCreateStatic/main.c
+++ training/01_Task_Creation/1_xTaskCreateStatic/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "sample_xTaskCreateStatic.h"
 
 /*-----------------------------------------------------------*/
@@ -6,5 +7,5 @@ int main( void )
 {
 	sample_xTaskCreateStatic();
 	
-	return 0;
+	exit(1);
 }
--- training/01_Task_Creation/2_xTaskCreate/main.c
+++ training/01_Task_Creation/2_xTaskCreate/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "sample_xTaskCreate.h"
 
 /*-----------------------------------------------------------*/
@@ -6,5 +7,5 @@ int main( void )
 {
 	sample_xTaskCreate();
 	
-	return 0;
+	exit(1);
 }
