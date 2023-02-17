962d71b941b016a72ea9372eb955d0f0d10ee995
Nishizaki <steelpipe75@gmail.com>
2023-02-18 01:32:13 +0900

    Update build_and_test.sh

--- build_and_test.sh
+++ build_and_test.sh
@@ -2,30 +2,18 @@
 
 #######################################
 
-cmake -B ./training/01_Task_Creation/1_xTaskCreateStatic/build -S ./training/01_Task_Creation/1_xTaskCreateStatic -DCMAKE_BUILD_TYPE=Release
-cmake --build ./training/01_Task_Creation/1_xTaskCreateStatic/build --config Release
-
-(
-    cd ./training/01_Task_Creation/1_xTaskCreateStatic/build
-    ctest -C Release -V
-)
-
-#######################################
-
-cmake -B ./training/01_Task_Creation/2_xTaskCreate/build -S ./training/01_Task_Creation/2_xTaskCreate -DCMAKE_BUILD_TYPE=Release
-cmake --build ./training/01_Task_Creation/2_xTaskCreate/build --config Release
-
-(
-    cd ./training/01_Task_Creation/2_xTaskCreate/build
-    ctest -C Release -V
-)
+cmake_build_test() {
+    echo $1
+    cmake -B $1/build -S $1 -DCMAKE_BUILD_TYPE=Release -G Ninja
+    cmake --build $1/build --config Release
+    (
+        cd $1/build
+        ctest -C Release -V
+    )
+}
 
 #######################################
 
-cmake -B ./training/02_Task_Control/2_vTaskDelayUntil/build -S ./training/02_Task_Control/2_vTaskDelayUntil -DCMAKE_BUILD_TYPE=Release
-cmake --build ./training/02_Task_Control/2_vTaskDelayUntil/build --config Release
-
-(
-    cd ./training/02_Task_Control/2_vTaskDelayUntil/build
-    ctest -C Release -V
-)
+cmake_build_test ./training/01_Task_Creation/1_xTaskCreateStatic
+cmake_build_test ./training/01_Task_Creation/2_xTaskCreate
+cmake_build_test ./training/02_Task_Control/2_vTaskDelayUntil
