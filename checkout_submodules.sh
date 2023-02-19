5bfe9cc5d46d852d9342c3109d28e967799dc366
Nishizaki <steelpipe75@gmail.com>
2023-02-19 08:00:02 +0000

    Github Codespaces でビルドを簡単に実行するための.shファイルを追加

---
+++ checkout_submodules.sh
@@ -0,0 +1,2 @@
+git config --global http.postBuffer 100M
+git submodule update --init --recursive --recommend-shallow --depth 1
---
+++ codespaces_build_and_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#######################################
+
+cmake_build_test() {
+    echo ==============================================================================
+    echo $1
+    echo ------------------------------------------------------------------------------
+    cmake -B $1/build -S $1 -DCMAKE_BUILD_TYPE=Release
+    echo ------------------------------------------------------------------------------
+    cmake --build $1/build --config Release
+    (
+        echo ------------------------------------------------------------------------------
+        cd $1/build
+        ctest -C Release -V
+    )
+    echo ------------------------------------------------------------------------------
+}
+
+#######################################
+
+cmake_build_test ./training/01_Task_Creation/1_xTaskCreateStatic
+cmake_build_test ./training/01_Task_Creation/2_xTaskCreate
+cmake_build_test ./training/02_Task_Control/1_vTaskDelay
+cmake_build_test ./training/02_Task_Control/2_vTaskDelayUntil
+cmake_build_test ./training/02_Task_Control/3_xTaskDelayUntil
