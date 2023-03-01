fbd4972414662b84d96eb51117c5bf660de69558
Nishizaki <steelpipe75@gmail.com>
2023-03-01 23:36:23 +0900

    Update main.go

--- utility/MyTrace2VCD/main.go
+++ utility/MyTrace2VCD/main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-type MyTrace struct {
+type MyTraceReadJson struct {
 	Type string
 	Data string
 }
@@ -22,7 +22,13 @@ type TaskSwitchedIn struct {
 	Out  string
 }
 
-func ReadJson(filename string) {
+type MyTrace struct {
+	Initialize          Initialize
+	TaskSwitchedInSlice []TaskSwitchedIn
+}
+
+func ReadJson(filename string) MyTrace {
+	var myTraceObj MyTrace
 	fp, err := os.Open(filename)
 	if err != nil {
 		panic(err)
@@ -30,36 +36,49 @@ func ReadJson(filename string) {
 	defer fp.Close()
 	scanner := bufio.NewScanner(fp)
 	for scanner.Scan() {
-		jsonObj := MyTrace{Type: "", Data: "0"}
+		jsonObj := MyTraceReadJson{Type: "", Data: "0"}
 		jsonStr := scanner.Text()
 		jsonByteArray := []byte(jsonStr)
 		if err := json.Unmarshal(jsonByteArray, &jsonObj); err != nil {
 			panic(err)
 		}
-		fmt.Println(jsonStr)
+		// fmt.Println(jsonStr)
 		if jsonObj.Type == "Initialize" {
 			initializeByteArray := []byte(jsonObj.Data)
 			initializeObj := Initialize{Timescale: -1.0}
 			if err := json.Unmarshal(initializeByteArray, &initializeObj); err != nil {
 				panic(err)
 			}
-			fmt.Printf("%+v\r\n", initializeObj)
+			myTraceObj.Initialize = initializeObj
+			// fmt.Printf("%+v\r\n", initializeObj)
 		} else if jsonObj.Type == "TaskSwitchedIn" {
 			taskSwitchedInByteArray := []byte(jsonObj.Data)
 			taskSwitchedInObj := TaskSwitchedIn{Tick: 0, In: "", Out: ""}
 			if err := json.Unmarshal(taskSwitchedInByteArray, &taskSwitchedInObj); err != nil {
 				panic(err)
 			}
-			fmt.Printf("%+v\r\n", taskSwitchedInObj)
+			myTraceObj.TaskSwitchedInSlice = append(myTraceObj.TaskSwitchedInSlice, taskSwitchedInObj)
+			// fmt.Printf("%+v\r\n", taskSwitchedInObj)
 		}
 	}
 
 	if err = scanner.Err(); err != nil {
 		panic(err)
 	}
+
+	return myTraceObj
+}
+
+func WriteVCD(filename string, myTraceObj MyTrace) {
+	fmt.Printf("%+v\r\n", myTraceObj.Initialize)
+	for _, v := range myTraceObj.TaskSwitchedInSlice {
+		fmt.Printf("%+v\r\n", v)
+	}
 }
 
 func main() {
 	json_filename := "example.log"
-	ReadJson(json_filename)
+	myTraceObj := ReadJson(json_filename)
+	vcd_filename := "example.vcd"
+	WriteVCD(vcd_filename, myTraceObj)
 }
