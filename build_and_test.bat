b3cd1f961792a986228c5e148c09a8e05a301b46
Nishizaki <steelpipe75@gmail.com>
2023-03-05 00:46:43 +0900

    フォルダ名を元々のDemoとそろえる

--- build_and_test.bat
+++ build_and_test.bat
@@ -6,18 +6,24 @@ call :cmake_build_test .\training\02_Task_Control\1_vTaskDelay
 call :cmake_build_test .\training\02_Task_Control\2_vTaskDelayUntil
 call :cmake_build_test .\training\02_Task_Control\3_xTaskDelayUntil
 
+call :cmake_build .\Demo_CMake\FreeRTOS_Plus_CLI_with_Trace_Windows_Simulator
+
 exit /b
 
 :cmake_build_test
+call :cmake_build %1
+pushd %1\build
+ctest -C Release -V
+popd
+echo ------------------------------------------------------------------------------
+exit /b
+
+:cmake_build
 echo ==============================================================================
 echo %1
 echo ------------------------------------------------------------------------------
 cmake -B %1\build -S %1 -DCMAKE_BUILD_TYPE=Release -G Ninja
 echo ------------------------------------------------------------------------------
 cmake --build %1\build --config Release
-echo ------------------------------------------------------------------------------
-pushd %1\build
-ctest -C Release -V
-popd
 echo ------------------------------------------------------------------------------
 exit /b
--- clean.bat
+++ clean.bat
@@ -6,6 +6,8 @@ call :clean_build .\training\02_Task_Control\1_vTaskDelay
 call :clean_build .\training\02_Task_Control\2_vTaskDelayUntil
 call :clean_build .\training\02_Task_Control\3_xTaskDelayUntil
 
+call :clean_build .\Demo_CMake\FreeRTOS_Plus_CLI_with_Trace_Windows_Simulator
+
 exit /b
 
 :clean_build
