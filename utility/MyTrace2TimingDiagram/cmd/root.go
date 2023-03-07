6d03bd7d2944b2e3d385789f20195eca46f12057
Nishizaki <steelpipe75@gmail.com>
2023-03-07 21:11:24 +0900

    Update root.go

--- utility/MyTrace2TimingDiagram/cmd/root.go
+++ utility/MyTrace2TimingDiagram/cmd/root.go
@@ -22,26 +22,32 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "MyTrace2TimingDiagram",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Convert from MyTraceLogData to PlantUML Timing Diagram",
+	Long:  `Convert from MyTraceLogData to PlantUML Timing Diagram`,
+	Run: func(cmd *cobra.Command, args []string) {
+		input, err := cmd.Flags().GetString("input")
+		if err != nil {
+			log.Fatal(err)
+		}
+		output, err := cmd.Flags().GetString("output")
+		if err != nil {
+			log.Fatal(err)
+		}
+		MyTrace2TimingDiagram(input, output)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -62,7 +68,84 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("input", "i", "example.log", "Help message for input")
+	rootCmd.Flags().StringP("output", "o", "example.puml", "Help message for output")
 }
 
+type MyTraceReadJson struct {
+	Type string
+	Data string
+}
 
+type Initialize struct {
+	Timescale float64
+}
+
+type TaskSwitch struct {
+	Tick int
+	In   string
+	Out  string
+}
+
+type MyTrace struct {
+	Initialize      Initialize
+	TaskSwitchSlice []TaskSwitch
+}
+
+func ReadLog(filename string) MyTrace {
+	fmt.Printf("Read log file (%s) start.\r\n", filename)
+	var myTraceObj MyTrace
+	fp, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		jsonObj := MyTraceReadJson{Type: "", Data: "0"}
+		jsonStr := scanner.Text()
+		jsonByteArray := []byte(jsonStr)
+		if err := json.Unmarshal(jsonByteArray, &jsonObj); err != nil {
+			panic(err)
+		}
+		// fmt.Println(jsonStr)
+		if jsonObj.Type == "Initialize" {
+			initializeByteArray := []byte(jsonObj.Data)
+			initializeObj := Initialize{Timescale: -1.0}
+			if err := json.Unmarshal(initializeByteArray, &initializeObj); err != nil {
+				panic(err)
+			}
+			myTraceObj.Initialize = initializeObj
+			// fmt.Printf("%+v\r\n", initializeObj)
+		} else if jsonObj.Type == "TaskSwitch" {
+			taskSwitchByteArray := []byte(jsonObj.Data)
+			taskSwitchObj := TaskSwitch{Tick: 0, In: "", Out: ""}
+			if err := json.Unmarshal(taskSwitchByteArray, &taskSwitchObj); err != nil {
+				panic(err)
+			}
+			taskSwitchObj.In = strings.Replace(taskSwitchObj.In, " ", "_", -1)
+			taskSwitchObj.Out = strings.Replace(taskSwitchObj.Out, " ", "_", -1)
+			myTraceObj.TaskSwitchSlice = append(myTraceObj.TaskSwitchSlice, taskSwitchObj)
+			// fmt.Printf("%+v\r\n", taskSwitchObj)
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Read log file (%s) done.\r\n", filename)
+	return myTraceObj
+}
+
+func WriteTimingDiagram(filename string, myTraceObj MyTrace) {
+	fmt.Printf("Write TimingDiagram file (%s) start.\r\n", filename)
+	// todo
+	fmt.Printf("Write TimingDiagram file (%s) done.\r\n", filename)
+}
+
+func MyTrace2TimingDiagram(json_filename string, td_filename string) {
+	myTraceObj := ReadLog(json_filename)
+	WriteTimingDiagram(td_filename, myTraceObj)
+}
